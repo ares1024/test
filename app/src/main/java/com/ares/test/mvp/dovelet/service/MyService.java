89e4ff199ce7b46b630cd3339122f305e09ad5e0
huangdi <huangdi@sensetime.com>
2017-12-12 09:59:42 +0800

    修改  使用3.0 最新构建

--- app/src/main/java/com/ares/test/mvp/dovelet/service/MyService.java
+++ app/src/main/java/com/ares/test/mvp/dovelet/service/MyService.java
@@ -4,21 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
-
-import com.ares.test.mvp.dovelet.IMyAidlInterface;
 
 public class MyService extends Service {
     public static final String TAG = MyService.class.getSimpleName();
     private IBinder mBinder = new MyBind();
-
-    IMyAidlInterface.Stub mBinderAIDL = new IMyAidlInterface.Stub() {
-        @Override
-        public void sum(int a, int b) throws RemoteException {
-            Log.d(TAG, "sum: " + (a + b));
-        }
-    };
 
     public MyService() {
     }
