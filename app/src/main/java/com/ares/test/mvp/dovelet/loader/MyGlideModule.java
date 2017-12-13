80775ea08a4c64333b811f3c6892974500b1a288
huangdi <huangdi@sensetime.com>
2017-12-13 17:00:59 +0800

    修改  使用glide4.4

---
+++ app/src/main/java/com/ares/test/mvp/dovelet/loader/MyGlideModule.java
@@ -0,0 +1,11 @@
+package com.ares.test.mvp.dovelet.loader;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by sensetime on 2017/12/13.
+ */
+@GlideModule
+public class MyGlideModule extends AppGlideModule {
+}
